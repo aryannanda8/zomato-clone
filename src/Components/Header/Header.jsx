b48ef98540befc469f2f8a3194ad40b319363463
Aryan Nanda <aryannanda97@gmail.com>
2024-05-11 13:13:56 +0530

    routes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import SearchBar from '../Common/SearchBar'
-
+import {useNavigate} from 'react-router-dom'
 function Header() {
+    
     return (
         < div className="bg-custom-image bg-cover bg-center  h-custom flex flex-col gap-y-16  flex-nowrap  items-center  text-white" >
 
@@ -16,7 +17,7 @@ function Header() {
             </nav>
             <div className='relative flex flex-col h-1/2 gap-10'>
                 <img src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png" alt="Zomato"
-                    className='object-contain h-14 w-full' />
+                    className='object-contain h-14 w-full ' />
                 <h2 className='text-4xl font-light'>Discover the best food & drinks in <b>Delhi NCR</b></h2>
                 {/* <div type="text" style={{ background: "white", border: "", height: "48px", width: "45rem", borderRadius: "10px" }} /> */}
               
--- src/Components/Header/HeaderMain.jsx
+++ src/Components/Header/HeaderMain.jsx
@@ -3,17 +3,22 @@ import './header.css'
 import { FaLocationDot, FaCaretDown, FaAngleDown } from "react-icons/fa6";
 import { CiSearch } from "react-icons/ci";
 import SearchBar from '../Common/SearchBar';
+import {useNavigate} from 'react-router-dom'
 function HeaderMain() {
+  let navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/')
+  }
   return (
     <div className='max-width header '>
 
-      <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="Zomato-logo" className='header-logo' />
+      <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="Zomato-logo" className='header-logo cursor-pointer'  onClick={handleClick}/>
 
       <div className='header-right h-[72px] flex flex-1 justify-between items-center'>
-        
-      
+
+
         <SearchBar size={1} />
-       
+
         <div className='profile-wrapper flex justify-evenly w-[20%] items-center text-sliderAngle text-[20px] font-[300]'>
           {/* <img src="https://b.zmtcdn.com/data/user_profile_pictures/8c0/a6b7213757758f5d031832af389b98c0.jpg?fit=around%7C100%3A100&amp;crop=100%3A100%3B%2A%2C%2A" alt="Profile image"
             className=' h-[40px] w-[40px] rounded-full' />
